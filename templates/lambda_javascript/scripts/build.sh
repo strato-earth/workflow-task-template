3c78fafc1edf4d7d7c5afc52096121ae322c9c9a
Kiril Kirov <kiril.t.kirov@gmail.com>
2023-06-24 17:36:07 -0600

    fix: flatten zip

--- templates/lambda_javascript/scripts/build.sh
+++ templates/lambda_javascript/scripts/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -eo pipefail
 
-pushd "$(dirname "$0")/.."
+pushd "$(dirname "$0")/.." > /dev/null
 ARTIFACT_FILE_NAME=${ARTIFACT_FILE_NAME:-artifact.zip}
-ARTIFACT_FILE_PATH=$(pwd)/$ARTIFACT_FILE_NAME
 npm run build
-zip $ARTIFACT_FILE_PATH dist/*
-popd
+pushd dist > /dev/null
+zip -r ../$ARTIFACT_FILE_NAME *
+popd > /dev/null
+popd > /dev/null

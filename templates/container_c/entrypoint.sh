b02a51460652347d5f2e509ef4cae222421c8a0a
Kiril Kirov <kiril.t.kirov@gmail.com>
2024-01-17 22:53:46 -0700

    feat: use entrypoint.sh for all docker templates

---
+++ templates/container_c/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -eo pipefail
+
+_error() {
+  # Custom Error Handling here
+  exit 1
+}
+
+trap "_error" ERR 
+
+# Set umask to ensure all newly created files have 777 permissions, so other users or services (like Lambda) can modify them 
+umask 000
+
+while [ "$1" != "" ]
+do
+    case $1 in
+        -action|-a   ) shift
+                        ACTION=$1
+                        ;;
+    esac
+    shift
+done
+
+# if [ "${ACTION}" = "" ]; then
+#   echo 'Usage : entrypoint.sh -a <action>'
+#   _error
+# fi
+
+# Pass all parameters to executable1
+"/usr/local/bin/executable1" "$@"
+
+echo "Done!"

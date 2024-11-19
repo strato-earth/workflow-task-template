8976ba5ed30f202871ac9db466b5aaccf396c050
Kiril Kirov <kiril.t.kirov@gmail.com>
2024-11-18 21:45:41 -0700

    feat: add pull permissions for ECR for workflow accounts

--- infrastructure/workflow-master/ecr.tf
+++ infrastructure/workflow-master/ecr.tf
@@ -6,3 +6,30 @@ resource "aws_ecr_repository" "main" {
     scan_on_push = true
   }
 }
+
+data "aws_iam_policy_document" "ecr_pull_policy" {
+  for_each = local.workflow_tasks_repos
+
+  statement {
+    sid    = "AllowPullAccessForWorkflowAccounts"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = local.workflow_accounts
+    }
+
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer"
+    ]
+  }
+}
+
+resource "aws_ecr_repository_policy" "main" {
+  for_each   = local.workflow_tasks_repos
+  repository = aws_ecr_repository.main[each.key].name
+
+  policy = data.aws_iam_policy_document.ecr_pull_policy[each.key].json
+}
